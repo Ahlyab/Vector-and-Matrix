9d8c79e7aa0f2477f3be0e969322aaf4cb377a96
Ahlyab <ahlyabasad@gmail.com>
2023-01-22 18:00:12 +0500

    Matrix Issue Fixed

--- Matrix.h
+++ Matrix.h
@@ -1,20 +1,20 @@
-// Matrix.h : interface for the Matrix class.
-
 #pragma once
-
 class Matrix
 {
 public:
     Matrix(const int row, const int col);            // parametric constructor
     void takeInput();                                // take input from the user and store in the matrix
-    void showAdditionResult(const Matrix &m2);       // show addition result if addition is possible
-    void showMultiplicationResult(const Matrix &m2); // show multiplication result if multiplication is possible
-    Matrix(const Matrix &m);                         // Copy Constructor
-    Matrix &operator=(const Matrix &m);              // assignment operator overloaded
+    void showAdditionResult(const Matrix& m2);       // show addition result if addition is possible
+    void showMultiplicationResult(const Matrix& m2); // show multiplication result if multiplication is possible
+    Matrix(const Matrix& m);                         // Copy Constructor
+    Matrix& operator=(const Matrix& m);              // assignment operator overloaded
     ~Matrix();                                       // destructor
+    void printMatrix();
+    void copyContent(const Matrix& m);
 
 private:
-    const int rows;
-    const int cols;
-    int **matrix;
+    int rows;
+    int cols;
+    int** matrix;
 };
+
